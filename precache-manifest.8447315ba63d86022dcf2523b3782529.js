e202795b9b0f6f8bc658aa03e0d6f9f53114894c
佐藤颯 <clicclic8088@gmail.com>
2020-03-02 18:38:32 +0900

    Updates

---
+++ precache-manifest.8447315ba63d86022dcf2523b3782529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7b88f114aff31b624d7afaa88ec71a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "857857bc73ebc2e468ee",
+    "url": "/monsters-rolodex/static/css/main.320a3728.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857857bc73ebc2e468ee",
+    "url": "/monsters-rolodex/static/js/main.83b9af5f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
